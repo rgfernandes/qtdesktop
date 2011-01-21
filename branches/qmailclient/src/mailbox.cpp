e266cd9bf6c7576416d21cffdb538344fe5a7c4b
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2011-01-21 18:21:36 +0000

    Read Mail.ru ok

--- branches/qmailclient/src/mailbox.cpp
+++ branches/qmailclient/src/mailbox.cpp
@@ -1,5 +1,10 @@
 #include <QDebug>
 
+#include <iostream>
+#include <sstream>
+#include <vector>
+#include <map>
+
 #include "mailbox.h"
 #include <vmime/platforms/posix/posixHandler.hpp>
 
@@ -20,6 +25,27 @@ bool	MailBox::setHandler(void) {
 bool	MailBox::setSession(void) {
 	session = vmime::create <vmime::net::session> ();
 	return true;
+}
+
+void	listFolder(vmime::ref <vmime::net::folder> folder) {
+	std::vector <vmime::ref <vmime::net::message> > allMessages = folder->getMessages();
+	//std::cerr << "Capabilities: " << folder->getFetchCapabilities();	// ENVELOPE, CONTENT_INFO, SIZE, FULL_HEADER, UID, IMPORTANCE
+	std::cerr << "Messages:" << folder->getMessageCount() << std::endl;
+	//folder->fetchMessages(allMessages, vmime::net::folder::FETCH_FLAGS|vmime::net::folder::FETCH_ENVELOPE);
+	folder->fetchMessages(allMessages, vmime::net::folder::FETCH_FULL_HEADER);
+	for (unsigned int i = 0; i < allMessages.size(); ++i) {
+		vmime::ref <vmime::net::message> msg = allMessages[i];
+		const int flags = msg->getFlags();
+		std::cerr << "Message " << i << " : " << std::endl;
+		if (flags & vmime::net::message::FLAG_SEEN)
+			std::cerr << " - is read" << std::endl;
+		if (flags & vmime::net::message::FLAG_DELETED)
+			std::cerr << " - is deleted" << std::endl;
+		//folder->fetchMessage(msg, vmime::net::folder::FETCH_FULL_HEADER);
+		vmime::ref <const vmime::header> hdr = msg->getHeader();
+		std::cerr << " - sent on " << hdr->Date()->generate() << std::endl;
+		std::cerr << " - sent by " << hdr->From()->generate() << std::endl;
+	}
 }
 
 bool	MailBox::setService(SERVICE service, QString &host, int port, QString &login, QString &password, CRYPTO crypto, AUTH sasl, AUTH apop) {
@@ -58,18 +84,25 @@ bool	MailBox::setService(SERVICE service, QString &host, int port, QString &logi
 		qDebug() << e.name();
 		qDebug() << e.what();
 	}
+	// list all folders
 	vmime::ref <vmime::net::folder> rootFolder = store->getRootFolder();
 	rootFolder->open(vmime::net::folder::MODE_READ_WRITE);
 	std::vector <vmime::ref <vmime::net::folder> > folders = rootFolder->getFolders(false);
 	for (unsigned int i = 0; i < folders.size(); i++) {
-		qDebug() << QString(folders[i]->getName());
+		std::cerr << folders[i]->getName().getBuffer() << std::endl;
 	}
+	// list msgs in INBOX
+	vmime::net::folder::path path;
+	path /= vmime::net::folder::path::component("INBOX");
+	vmime::ref <vmime::net::folder> inbox = store->getFolder(path);
+	inbox->open(vmime::net::folder::MODE_READ_WRITE);
+	listFolder(inbox);
 	//qDebug() << url;
 	return true;
 }
 
-bool	MailBox::setTransport(QString &host, int port, QString login, QString password, CRYPTO crypto, AUTH pass, AUTH sasl) {
-	return true;
-}
+//bool	MailBox::setTransport(QString &host, int port, QString login, QString password, CRYPTO crypto, AUTH pass, AUTH sasl) {
+//	return true;
+//}
 
 QStringList	getFolders(void);
