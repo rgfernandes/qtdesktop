8c1765f676b5a2afe31cd0d30690d7e13dfa8419
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2011-01-25 12:26:47 +0000

    Accounts model starts work

--- branches/qmailclient/src/accountsdialogimpl.cpp
+++ branches/qmailclient/src/accountsdialogimpl.cpp
@@ -8,33 +8,56 @@
 
 #include "accountsdialogimpl.h"
 
+AccountsModel::AccountsModel(QSettings * s, QObject * parent) :
+	settings(s) {
+	lv = settings->childGroups();
+}
+
+AccountsModel::~AccountsModel() {
+}
+
+void AccountsModel::reloadsettings() const {
+	//lv = settings->childGroups();
+}
+
+int AccountsModel::rowCount(const QModelIndex & parent) const {
+	reloadsettings();
+	return lv.size();
+}
+
+QVariant AccountsModel::data(const QModelIndex & index, int role) const {
+	if (role == Qt::DisplayRole) {
+		//qDebug() << index.row();
+		return QVariant(lv[index.row()]);
+	} else {
+		return QVariant();
+	}
+}
+// ====
 AccountsDialogImpl::AccountsDialogImpl(QWidget *parent) 
-	: QDialog(parent)
-{
+	: QDialog(parent) {
 	setupUi(this);
-	QSettings s(QSettings::IniFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName());
-	QStringList grouplist = s.childGroups();
+	settings = new QSettings(QSettings::IniFormat, QSettings::UserScope, QCoreApplication::organizationName(), QCoreApplication::applicationName());
+	AccountsModel *am = new AccountsModel(settings);
+	lvAccounts->setModel(am);
+	/*QStringList grouplist = s.childGroups();
 	for (int i = 0; i < grouplist.size(); i++) {
 		qDebug() << grouplist[i];
-	}
+	}*/
 	//connect(this,SIGNAL(accepted()),this,SLOT(writeSettings()));
 }
 
-AccountsDialogImpl::~AccountsDialogImpl()
-{
+AccountsDialogImpl::~AccountsDialogImpl() {
 }
 
-void AccountsDialogImpl::on_pbAdd_clicked()
-{
+void AccountsDialogImpl::on_pbAdd_clicked() {
 	qDebug() << "Add clicked";
 }
 
-void AccountsDialogImpl::on_pbEdit_clicked()
-{
+void AccountsDialogImpl::on_pbEdit_clicked() {
 	qDebug() << "Edit clicked";
 }
 
-void AccountsDialogImpl::on_pbDel_clicked()
-{
+void AccountsDialogImpl::on_pbDel_clicked() {
 	qDebug() << "Del clicked";
 }
--- branches/qmailclient/src/accountsdialogimpl.h
+++ branches/qmailclient/src/accountsdialogimpl.h
@@ -3,16 +3,32 @@
 
 #include "ui_AccountsDialog.h"
 
-class AccountsDialogImpl : public QDialog, public Ui::AccountsDialog
-{
+#include <QSettings>
+
+class AccountsModel : public QAbstractListModel {
+	public:
+		AccountsModel(QSettings *, QObject * parent = 0);
+		~AccountsModel();
+		int rowCount(const QModelIndex & parent = QModelIndex()) const;
+		QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+		void reloadsettings() const; 
+		//headerData()
+	private:
+		QSettings *settings;
+		QStringList lv;
+};
+
+class AccountsDialogImpl : public QDialog, public Ui::AccountsDialog {
 Q_OBJECT
 public:
-	AccountsDialogImpl(QWidget *parent=0);
+	AccountsDialogImpl(QWidget * parent=0);
 	~AccountsDialogImpl();
 private slots:
 	void on_pbAdd_clicked();
 	void on_pbEdit_clicked();
 	void on_pbDel_clicked();
+private:
+	QSettings *settings;
 };
 
 #endif // __ACCOUNTSDIALOGIMPL_H__
