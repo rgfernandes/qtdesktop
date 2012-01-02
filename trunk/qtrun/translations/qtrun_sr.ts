538d01581c8f5ad59d70e72249c9fb9549b5cbea
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-01-02 13:52:49 +0000

    Serbian translations added

---
+++ trunk/qtrun/translations/qtrun_sr.ts
@@ -0,0 +1,27 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr">
+<context>
+    <name>QtRun</name>
+    <message>
+        <location filename="../qtrun.cpp" line="30"/>
+        <source>Run application</source>
+        <translation>Покрени апликацију</translation>
+    </message>
+    <message>
+        <location filename="../qtrun.cpp" line="34"/>
+        <source>Browse</source>
+        <translation>Потражи</translation>
+    </message>
+    <message>
+        <location filename="../qtrun.cpp" line="48"/>
+        <source>Open executable</source>
+        <translation>Отвори извршни фајл</translation>
+    </message>
+    <message>
+        <location filename="../qtrun.cpp" line="48"/>
+        <source>All (*.*)</source>
+        <translation>Све (*.*)</translation>
+    </message>
+</context>
+</TS>
