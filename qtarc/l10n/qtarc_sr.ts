61077347d3e35ef71b717a4ef6b52de7e60b3c62
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-01-02 13:52:49 +0000

    Serbian translations added

---
+++ qtarc/l10n/qtarc_sr.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr">
+<context>
+    <name>ArchItemModel</name>
+    <message>
+        <location filename="../src/architemmodel.cpp" line="88"/>
+        <source>Name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location filename="../src/architemmodel.cpp" line="91"/>
+        <source>Size</source>
+        <translation>Величина</translation>
+    </message>
+    <message>
+        <location filename="../src/architemmodel.cpp" line="94"/>
+        <source>Date</source>
+        <translation>Датум</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="47"/>
+        <source>&amp;File</source>
+        <translation>&amp;Фајл</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="59"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Уреди</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="70"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Помоћ</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="77"/>
+        <source>&amp;Actions</source>
+        <translation>&amp;Радње</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="92"/>
+        <source>ToolBar</source>
+        <translation>Трака алата</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="114"/>
+        <source>&amp;New</source>
+        <translation>Но&amp;ви</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="129"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Отвори...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="159"/>
+        <source>&amp;Extract</source>
+        <translation>&amp;Распакуј</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="174"/>
+        <source>&amp;Properties</source>
+        <translation>&amp;Својства</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="144"/>
+        <source>Save &amp;As...</source>
+        <translation>Сачувај &amp;као...</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="185"/>
+        <source>E&amp;xit</source>
+        <translation>&amp;Напусти</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="200"/>
+        <source>Add &amp;File</source>
+        <translation>Додај &amp;фајл</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="215"/>
+        <source>Add &amp;Directory</source>
+        <translation>Додај фа&amp;сциклу</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="230"/>
+        <source>Cut</source>
+        <translation>Исеци</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="245"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Копирај</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="260"/>
+        <source>Paste</source>
+        <translation>Налепи</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="275"/>
+        <source>Delete</source>
+        <translation>Обриши</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="290"/>
+        <source>About</source>
+        <translation>О програму</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="301"/>
+        <source>AboutQt</source>
+        <translation>О Кјуту</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="309"/>
+        <source>Select All</source>
+        <translation>Изабери све</translation>
+    </message>
+    <message>
+        <location filename="../ui/mainwindow.ui" line="320"/>
+        <source>Deselect</source>
+        <translation>Поништи избор</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindowImpl</name>
+    <message>
+        <location filename="../src/mainwindowimpl.cpp" line="34"/>
+        <source>Open file</source>
+        <translation>Отвори фајл</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindowimpl.cpp" line="34"/>
+        <source>Archive</source>
+        <translation>Архива</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindowimpl.cpp" line="69"/>
+        <source>Add file(s)</source>
+        <translation>Додај фајл(ове)</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindowimpl.cpp" line="77"/>
+        <location filename="../src/mainwindowimpl.cpp" line="88"/>
+        <source>Add directory</source>
+        <translation>Додај фасциклу</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindowimpl.cpp" line="105"/>
+        <source>About</source>
+        <translation>О програму</translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindowimpl.cpp" line="109"/>
+        <source>About Qt</source>
+        <translation>О Кјуту</translation>
+    </message>
+</context>
+</TS>
---
+++ qtpdfview/l10n/qtpdfview_sr.ts
@@ -0,0 +1,366 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr">
+<context>
+    <name>EmbeddedFilesDock</name>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="30"/>
+        <source>Embedded files</source>
+        <translation>Уграђени фајлови</translation>
+    </message>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="33"/>
+        <location filename="../src/embeddedfiles.cpp" line="45"/>
+        <source>Name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="33"/>
+        <location filename="../src/embeddedfiles.cpp" line="45"/>
+        <source>Description</source>
+        <translation>Опис</translation>
+    </message>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="33"/>
+        <location filename="../src/embeddedfiles.cpp" line="45"/>
+        <source>Size</source>
+        <translation>Величина</translation>
+    </message>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="33"/>
+        <location filename="../src/embeddedfiles.cpp" line="45"/>
+        <source>Creation date</source>
+        <translation>Датум прављења</translation>
+    </message>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="34"/>
+        <location filename="../src/embeddedfiles.cpp" line="46"/>
+        <source>Modification date</source>
+        <translation>Датум измене</translation>
+    </message>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="34"/>
+        <location filename="../src/embeddedfiles.cpp" line="46"/>
+        <source>Checksum</source>
+        <translation>Контролна сума</translation>
+    </message>
+    <message>
+        <location filename="../src/embeddedfiles.cpp" line="48"/>
+        <source>No files</source>
+        <translation>Нема фајлова</translation>
+    </message>
+</context>
+<context>
+    <name>FontsDock</name>
+    <message>
+        <location filename="../src/fonts.cpp" line="35"/>
+        <source>Fonts</source>
+        <translation>Фонтови</translation>
+    </message>
+    <message>
+        <location filename="../src/fonts.cpp" line="37"/>
+        <location filename="../src/fonts.cpp" line="48"/>
+        <source>Name</source>
+        <translation>Име</translation>
+    </message>
+    <message>
+        <location filename="../src/fonts.cpp" line="37"/>
+        <location filename="../src/fonts.cpp" line="48"/>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+    <message>
+        <location filename="../src/fonts.cpp" line="37"/>
+        <location filename="../src/fonts.cpp" line="48"/>
+        <source>Embedded</source>
+        <translation>Уграђено</translation>
+    </message>
+    <message>
+        <location filename="../src/fonts.cpp" line="37"/>
+        <location filename="../src/fonts.cpp" line="48"/>
+        <source>Subset</source>
+        <translation>Подсет</translation>
+    </message>
+    <message>
+        <location filename="../src/fonts.cpp" line="37"/>
+        <location filename="../src/fonts.cpp" line="48"/>
+        <source>File</source>
+        <translation>Фајл</translation>
+    </message>
+</context>
+<context>
+    <name>InfoDock</name>
+    <message>
+        <location filename="../src/info.cpp" line="30"/>
+        <source>Information</source>
+        <translation>Информације</translation>
+    </message>
+    <message>
+        <location filename="../src/info.cpp" line="32"/>
+        <location filename="../src/info.cpp" line="43"/>
+        <source>Key</source>
+        <translation>Кључ</translation>
+    </message>
+    <message>
+        <location filename="../src/info.cpp" line="32"/>
+        <location filename="../src/info.cpp" line="43"/>
+        <source>Value</source>
+        <translation>Вриједност</translation>
+    </message>
+</context>
+<context>
+    <name>MetadataDock</name>
+    <message>
+        <location filename="../src/metadata.cpp" line="30"/>
+        <source>Metadata</source>
+        <translation>Метаподаци</translation>
+    </message>
+</context>
+<context>
+    <name>NavigationToolBar</name>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="29"/>
+        <source>First</source>
+        <translation>Прва</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="30"/>
+        <source>Previous</source>
+        <translation>Претходна</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="34"/>
+        <source>Next</source>
+        <translation>Следећа</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="35"/>
+        <source>Last</source>
+        <translation>Последња</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="41"/>
+        <source>10%</source>
+        <translation>10%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="42"/>
+        <source>25%</source>
+        <translation>25%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="43"/>
+        <source>33%</source>
+        <translation>33%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="44"/>
+        <source>50%</source>
+        <translation>50%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="45"/>
+        <source>66%</source>
+        <translation>66%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="46"/>
+        <source>75%</source>
+        <translation>75%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="47"/>
+        <source>100%</source>
+        <translation>100%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="48"/>
+        <source>125%</source>
+        <translation>125%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="49"/>
+        <source>150%</source>
+        <translation>150%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="50"/>
+        <source>200%</source>
+        <translation>200%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="51"/>
+        <source>300%</source>
+        <translation>300%</translation>
+    </message>
+    <message>
+        <location filename="../src/navigationtoolbar.cpp" line="52"/>
+        <source>400%</source>
+        <translation>400%</translation>
+    </message>
+</context>
+<context>
+    <name>OptContentDock</name>
+    <message>
+        <location filename="../src/optcontent.cpp" line="30"/>
+        <source>Optional content</source>
+        <translation>Необавезни садржај</translation>
+    </message>
+</context>
+<context>
+    <name>PdfViewer</name>
+    <message>
+        <location filename="../src/viewer.cpp" line="48"/>
+        <source>Poppler-Qt4 Demo</source>
+        <translation>Поплер-Кјут4 демо</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="51"/>
+        <source>&amp;File</source>
+        <translation>&amp;Фајл</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="52"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Отвори</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="55"/>
+        <source>&amp;Save a Copy...</source>
+        <translation>Сачувај &amp;копију...</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="59"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Напусти</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="62"/>
+        <source>&amp;View</source>
+        <translation>&amp;Приказ</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="64"/>
+        <source>&amp;Settings</source>
+        <translation>П&amp;одешавање</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="65"/>
+        <source>Text Antialias</source>
+        <translation>Омекшавање ивица текста</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="68"/>
+        <source>Graphics Antialias</source>
+        <translation>Омекшавање ивица графике</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="71"/>
+        <source>Render Backend</source>
+        <translation>Мотор исцртавања</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="74"/>
+        <source>Splash</source>
+        <translation>Сплеш</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="79"/>
+        <source>Arthur</source>
+        <translation>Артур</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="86"/>
+        <source>&amp;Help</source>
+        <translation>По&amp;моћ</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="87"/>
+        <source>&amp;About</source>
+        <translation>&amp;О програму</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="88"/>
+        <source>About &amp;Qt</source>
+        <translation>О К&amp;јуту</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="171"/>
+        <source>Open Error</source>
+        <translation>Грешка при отварању</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="171"/>
+        <source>Cannot open:
+</source>
+        <translation>Не могу да отворим:</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="179"/>
+        <source>Document Password</source>
+        <translation>Лозинка документа</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="180"/>
+        <source>Please insert the password of the document:</source>
+        <translation>Унесите лозинку документа:</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="224"/>
+        <source>Open PDF Document</source>
+        <translation>Отвори ПДФ документ</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="224"/>
+        <location filename="../src/viewer.cpp" line="238"/>
+        <source>PDF Documents (*.pdf)</source>
+        <translation>ПДФ документи (*.pdf)</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="238"/>
+        <source>Save Copy</source>
+        <translation>Сачувај копију</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="247"/>
+        <source>Save Error</source>
+        <translation>Грешка при упису</translation>
+    </message>
+    <message>
+        <location filename="../src/viewer.cpp" line="247"/>
+        <source>Cannot export to:
+%1</source>
+        <translation>Не могу да извезем у:
+%1</translation>
+    </message>
+</context>
+<context>
+    <name>PermissionsDock</name>
+    <message>
+        <location filename="../src/permissions.cpp" line="30"/>
+        <source>Permissions</source>
+        <translation>Дозволе</translation>
+    </message>
+</context>
+<context>
+    <name>ThumbnailsDock</name>
+    <message>
+        <location filename="../src/thumbnails.cpp" line="33"/>
+        <source>Thumbnails</source>
+        <translation>Сличице</translation>
+    </message>
+</context>
+<context>
+    <name>TocDock</name>
+    <message>
+        <location filename="../src/toc.cpp" line="61"/>
+        <source>TOC</source>
+        <translation>Садржај</translation>
+    </message>
+    <message>
+        <location filename="../src/toc.cpp" line="76"/>
+        <source>No TOC</source>
+        <translation>Нема</translation>
+    </message>
+</context>
+</TS>
