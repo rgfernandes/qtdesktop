3aa3b78acee17ef2e47f35ddb4af8f2b815b6b7f
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-11-05 14:08:31 +0000

    flush

--- trunk/winrepo/bin/mk_any.sh
+++ trunk/winrepo/bin/mk_any.sh
@@ -5,11 +5,28 @@
 MINGW=/usr/i686-w64-mingw32/sys-root/mingw
 VER=`rpm -qi mingw32-$1 | grep ^Version | gawk '{print $3}'`
 REL=`rpm -qi mingw32-$1 | grep ^Release | gawk '{print $3}'| sed 's/\.fc17//'`
-DEST=`pwd`/QDE-$1-$VER-$REL.7z
-TMP=`mktemp -d`
-pushd $TMP
+DEST=`pwd`/QDE-$1-$VER-$REL.zip
+TMPDIR=`mktemp -d`
+pushd $TMPDIR
 mkdir lib
-for i in `rpm -ql mingw32-$1 | grep dll$`; do cp $i lib/; done
-7z a $DEST lib/*
+# 1. cp files
+for i in `rpm -ql mingw32-$1 | grep dll$`
+do
+    cp $i lib/
+done
+# 2. prepare [Un]install scripts
+TMPINSTALL=`mktemp`
+TMPUNINSTALL=`mktemp`
+find . -type f | sed 's/^\.\///' | sed 's/\//\\\\/' | while read i
+do
+    echo "MOVE $i %SystemDrive%\\QtDesktop\\lib\\" >> $TMPINSTALL
+    echo "DEL %SystemDrive%\\QtDesktop\\$i" >> $TMPUNINSTALL
+done
+echo "RMDIR lib" >> $TMPINSTALL
+mv $TMPINSTALL Install.bat
+mv $TMPUNINSTALL Uninstall.bat
+unix2dos Install.bat Uninstall.bat
+# 3. pack
+zip $DEST *
 popd
-rm -rf $TMP
+rm -rf $TMPDIR
