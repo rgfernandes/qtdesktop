8d032d147844adca181ac4c63313c9b7ad0ed413
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-11-05 15:04:52 +0000

    flush

--- winrepo/bin/mk_any.sh
+++ winrepo/bin/mk_any.sh
@@ -5,7 +5,7 @@
 MINGW=/usr/i686-w64-mingw32/sys-root/mingw
 VER=`rpm -qi mingw32-$1 | grep ^Version | gawk '{print $3}'`
 REL=`rpm -qi mingw32-$1 | grep ^Release | gawk '{print $3}'| sed 's/\.fc17//'`
-DEST=`pwd`/QDE-$1-$VER-$REL.zip
+DEST=`pwd`/QtDesktop-$1-$VER-$REL.zip
 TMPDIR=`mktemp -d`
 pushd $TMPDIR
 mkdir lib
@@ -19,8 +19,8 @@ TMPINSTALL=`mktemp`
 TMPUNINSTALL=`mktemp`
 find . -type f | sed 's/^\.\///' | sed 's/\//\\\\/' | while read i
 do
-    echo "MOVE $i %SystemDrive%\\QtDesktop\\lib\\" >> $TMPINSTALL
-    echo "DEL %SystemDrive%\\QtDesktop\\$i" >> $TMPUNINSTALL
+    echo "MOVE $i %QTDESKTOP%\\lib\\" >> $TMPINSTALL
+    echo "DEL %QTDESKTOP%\\$i" >> $TMPUNINSTALL
 done
 echo "RMDIR lib" >> $TMPINSTALL
 mv $TMPINSTALL Install.bat
