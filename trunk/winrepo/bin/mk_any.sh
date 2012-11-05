1c426e1d84d57f38845871d9cb1291ae90995070
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-11-05 16:05:28 +0000

    flush

--- trunk/winrepo/bin/mk_any.sh
+++ trunk/winrepo/bin/mk_any.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Make QDE-* package
-# $1 - name (gcc, gcc-c++, libpng
+# $1 - name (gcc, gcc-c++, zlib, libpng, qscintilla)
 # E.g. ./mk_any.sh gcc
 MINGW=/usr/i686-w64-mingw32/sys-root/mingw
 VER=`rpm -qi mingw32-$1 | grep ^Version | gawk '{print $3}'`
@@ -8,8 +8,8 @@ REL=`rpm -qi mingw32-$1 | grep ^Release | gawk '{print $3}'| sed 's/\.fc17//'`
 DEST=`pwd`/QtDesktop-$1-$VER-$REL.zip
 TMPDIR=`mktemp -d`
 pushd $TMPDIR
-mkdir lib
 # 1. cp files
+mkdir lib
 for i in `rpm -ql mingw32-$1 | grep dll$`
 do
     cp $i lib/
--- trunk/winrepo/bin/mk_qt.sh
+++ trunk/winrepo/bin/mk_qt.sh
@@ -1,13 +1,33 @@
 #!/bin/sh
 # Make QDE-Qt4 package
 MINGW=/usr/i686-w64-mingw32/sys-root/mingw
-DEST=`pwd`/QDE-Qt4-4.8.3-1.7z
+DEST=`pwd`/QtDesktop-Qt4-4.8.3-1.zip
 TMP=`mktemp -d`
 pushd $TMP
+# 1. cp files
 mkdir lib
 cp $MINGW/bin/Q*.dll lib/
-cp -r $MINGW/lib/qt4 lib/
+cp -r $MINGW/lib/qt4 .
 find . -type f -name "*d4.dll" -delete
-7z a $DEST lib/*
+# 2. prepare [Un]install scripts
+TMPINSTALL=`mktemp`
+TMPUNINSTALL=`mktemp`
+find lib -type f | sed 's/\//\\\\/' | while read i
+do
+    echo "MOVE $i \"%QTDESKTOP%\\$i\"" >> $TMPINSTALL
+    echo "DEL \"%QTDESKTOP%\\$i\"" >> $TMPUNINSTALL
+done
+echo "MOVE qt4 \"%QTDESKTOP%\\qt4\"" >> $TMPINSTALL
+echo "RMDIR lib" >> $TMPINSTALL
+echo "REG ADD \"HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\" /v QTDIR /t REG_SZ /d \"%QTDESKTOP%\\qt4\" /f" >> $TMPINSTALL
+echo "RMDIR /S /Q \"%QTDESKTOP%\\qt4\"" >> $TMPUNINSTALL
+echo "REG DELETE \"HKLM\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment\" /v QTDIR /f" >> $TMPUNINSTALL
+unix2dos $TMPINSTALL $TMPUNINSTALL
+mkdir .Npackd
+mv $TMPINSTALL .Npackd/Install.bat
+mv $TMPUNINSTALL .Npackd/Uninstall.bat
+# 3. pack
+zip -r $DEST lib/* qt4 .Npackd/*
 popd
 rm -rf $TMP
+sha1sum $DEST
