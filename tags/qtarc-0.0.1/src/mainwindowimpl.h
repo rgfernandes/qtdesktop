2968569e508aeb9916f5c9c3a555590357c7f74a
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-11-17 17:43:27 +0000

    trunk/qtarc copied to tags/qtarc-0.0.1

---
+++ tags/qtarc-0.0.1/src/mainwindowimpl.h
@@ -0,0 +1,35 @@
+#ifndef MAINWINDOWIMPL_H
+#define MAINWINDOWIMPL_H
+
+#include <QMainWindow>
+#include "ui_mainwindow.h"
+#include "architemmodel.h"
+
+class MainWindowImpl : public QMainWindow, public Ui::MainWindow
+{
+Q_OBJECT
+public:
+	MainWindowImpl( QWidget * parent = 0, Qt::WFlags f = 0 );
+private slots:
+	void		onActionNew(void);
+	void		onActionOpen(void);
+	void		onActionSaveAs(void);
+	void		onActionProperties(void);
+	void		onActionExit(void);
+	void		onActionCut(void);
+	void		onActionCopy(void);
+	void		onActionPaste(void);
+	void		onActionSelectAll(void);
+	void		onActionDeselect(void);
+	void		onActionAddFile(void);
+	void		onActionAddDirectory(void);
+	void		onActionExtract(void);
+	void		onActionDelete(void);
+	void		onActionAbout(void);
+	void		onActionAboutQt(void);
+private:
+	void		setSlots(void);
+	Archive		*archive;
+};
+
+#endif
