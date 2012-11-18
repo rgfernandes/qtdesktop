791da3b9494b46afd0a31cc05eebf13df3869043
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-11-18 16:50:14 +0000

    flush

--- pyqtarc/src/archfile.py
+++ pyqtarc/src/archfile.py
@@ -1,1 +1,18 @@
+import architem
 
+class	ArchFile:
+	'''
+	Archive file representation
+	'''
+
+	def	__init__(self):
+		self.__root = architem.ArchItemFolder("")
+
+	def	sort(self):
+		self.__root.getChildren().sort()
+
+	def	load(self,  path):
+		pass
+
+	def	List(self):
+		return self.__root.getChildren()
