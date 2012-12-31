91a886551675923d1b52f70cee7b2d24901f2ff5
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-12-31 11:56:08 +0000

    add file ok

--- trunk/pyqtarc/src/archfile.py
+++ trunk/pyqtarc/src/archfile.py
@@ -1,6 +1,7 @@
 from PyQt4 import QtCore
 import architem
 from helper.lzma import ArchHelper7z
+import os
 
 class	ArchFile:
 	'''
@@ -9,18 +10,51 @@ class	ArchFile:
 
 	def	__init__(self):
 		self.__root = architem.ArchItemFolder("")
+		self.__file = None
+		self.__helper = None
 
+	# from model interface
 	def	getRoot(self):
 		return self.__root
 
 	def	sort(self):
 		self.__root.getChildren().sort()
 
-	def	load(self, path):
-		helper = ArchHelper7z()
-		for i in helper.list(path):
-			self.__root.addChildRecursive(i[0].split("/"), i[1], QtCore.QDateTime(i[2]), i[3])	# name:str, isdir:bool, mtime:datetime, size:int
-		self.__root.getChildren().sort()
-
 	def	List(self):
 		return self.__root.getChildren()
+
+	# from main window interface
+	def	__list(self, result):
+		'''
+		TODO: err handling
+		'''
+		for i in result:
+			# name:str, isdir:bool, mtime:datetime, size:int
+			self.__root.addChildRecursive(i[0].split("/"), i[1], QtCore.QDateTime(i[2]), i[3])
+		self.__root.getChildren().sort()
+
+	def	load(self, path):
+		'''
+		TODO: choosing helper
+		'''
+		self.__file = path
+		self.__helper = ArchHelper7z()
+		errcode, result = self.__helper.list(path)
+		if (errcode):
+			return
+		return self.__list(result)
+
+	def	add(self, path):
+		'''
+		get file name
+		try to add
+		get file info
+		add ArchItemFile
+		TODO: same as load
+		TODO: folder
+		'''
+		f = QtCore.QFileInfo(path)
+		errcode, result = self.__helper.add(self.__file, path)
+		if errcode:
+			return
+		return self.__list(result)
