0b0599e86f835d5ceb0827f0849c4a42c597e095
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2012-12-31 11:56:08 +0000

    add file ok

--- pyqtarc/src/helper/lzma.py
+++ pyqtarc/src/helper/lzma.py
@@ -1,13 +1,13 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 '''
-7z helper
+7z helper. Load from archfile.ArchFile.load()
 TODO:
 * One helper class+
 * 5 callbacks(str) - name, isdir, mtime, size, csize
 '''
 
-import sys, re, datetime, subprocess, pprint
+import sys, os, re, datetime, subprocess, pprint
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -16,28 +16,40 @@ class	ArchHelper7z:
 	exts = ('7z',)
 	mimes = ('application/x-7z-compressed',)
 
+	def	__init__(self):
+		self.__rx = re.compile("\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [D.][R.][H.][S.][A.] [ 0-9]{12} [ 0-9]{12}  .*\n")
+
 	@classmethod
 	def	get_ext():
+		'''
+		Get file extensions supported
+		@return: extensions
+		@rtype: list
+		'''
 		return self.exts
 
 	@classmethod
 	def	get_mime():
+		'''
+		Get file mimetypes supported
+		@return: extensions
+		@rtype: list
+		'''
 		return self.mimes
 
-	def	__init__(self):
-		self.__rx = re.compile("\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [D.][R.][H.][S.][A.] [ 0-9]{12} [ 0-9]{12}  .*\n")
-
-	def	list(self, path):
+	def	list(self, path, files=tuple()):
 		'''
+		List archive.
+		TODO: err handling: str, errcode
 		@param path: full path to archive
 		@type path: str
-		@param root: where to load to
-		@type root: ArchItemFolder
 		@return: (name, isdir, mtime, size, csize)
 		@rtype: list
 		'''
-		p = subprocess.Popen(["7za", "l", path], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		p = subprocess.Popen(["7za", "l", path] + list(files), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		out, err = p.communicate()
+		if (p.returncode):
+			return (p.returncode, err)
 		retvalue = list()
 		for match in self.__rx.finditer(out):
 			s = match.group().rstrip("\n")
@@ -46,10 +58,26 @@ class	ArchHelper7z:
 					(s[20] == 'D'),		# isdir:bool
 					datetime.datetime.strptime(s[:19], "%Y-%m-%d %H:%M:%S"),	# mtime:datetime
 					long(s[26:38].lstrip()),	# size:long
-					long(s[39:51].lstrip()) if s[39:51].lstrip() else 0,	# csize:long
+					long(s[39:51].lstrip()) if s[39:51].lstrip() else 0,		# csize:long
 				)
 			)
-		return retvalue
+		return (0, retvalue)
+
+	def	add(self, apath, fpath):
+		'''
+		@param apath: full path to archive
+		@type path: str
+		@param fpath: full path to file/folder to add
+		@type fpath: str
+		@return: errcode, errmsg
+		@rtype: (int, str)
+		'''
+		p = subprocess.Popen(["7za", "a", apath, fpath], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = p.communicate()
+		if (p.returncode):
+			return (p.returncode, err)
+		fname = os.path.basename(str(fpath))
+		return self.list(str(apath), (fname,))
 
 if __name__ == '__main__':
 	helper = ArchHelper7z()
--- pyqtarc/src/mainwindow.py
+++ pyqtarc/src/mainwindow.py
@@ -1,3 +1,4 @@
+# filter: 
 from PyQt4 import QtCore, QtGui
 from ui.Ui_main import Ui_Main
 
@@ -15,14 +16,28 @@ class	MainWindow(QtGui.QMainWindow, Ui_Main):
 
 	def	__setSlots(self):
 		self.connect(self.action_FileOpen,	QtCore.SIGNAL( "triggered()" ), self.__onActionFileOpen )
+		self.connect(self.action_AddFile,	QtCore.SIGNAL( "triggered()" ), self.__onActionAddFile )
+		#self.connect(self.action_AddDirectory,	QtCore.SIGNAL( "triggered()" ), self.__onActionAddFolder )
 
 	def	__onActionFileOpen(self):
+		'''
+		filter " (*.7z *.arj *.rar *.zip *.tar *.tar.gz *.tgz *.tar.bz *.tar.bz2 *.tbz *.tbz2  *.tbz *.tar.7z *.tar.lzma *.tlzma *.tar.xz *.txz)")
+		'''
 		fileName = QtGui.QFileDialog.getOpenFileName(
 			parent = None,
 			caption=self.tr("Open file"),
 			directory="",
-			# " (*.7z *.arj *.rar *.zip *.tar *.tar.gz *.tgz *.tar.bz *.tar.bz2 *.tbz *.tbz2  *.tbz *.tar.7z *.tar.lzma *.tlzma *.tar.xz *.txz)")
 			filter = self.tr("Archive") + " (*.7z)")
 		if (not fileName.isEmpty()):
 			self.__archfile.load(fileName)
+			self.treeView.model().refresh()
+
+	def	__onActionAddFile(self):
+		fileName = QtGui.QFileDialog.getOpenFileName(
+			parent = None,
+			caption=self.tr("Add file"),
+			directory="",
+			filter = self.tr("Any file") + " (*.*)")
+		if (not fileName.isEmpty()):
+			self.__archfile.add(fileName)
 			self.treeView.model().refresh()
