1dc485ef83c8f1a6630b5a8358f342a070084137
ti.eugene <ti.eugene@d35ce407-1151-0410-88f3-85c566f0df54>
2013-02-03 10:43:19 +0000

    lzma selftest, zip selftest, zip qt-based helper

--- pyqtarc/src/helper/zip.py
+++ pyqtarc/src/helper/zip.py
@@ -1,10 +1,11 @@
 #!/bin/env python
 # -*- coding: utf-8 -*-
 '''
-7z helper. Load from archfile.ArchFile.load()
+ZIP helper
 '''
 
-import sys, os, re, datetime, subprocess, pprint
+import sys
+from PyQt4 import QtCore
 from base import *
 
 reload(sys)
@@ -16,40 +17,33 @@ mimes = ('application/x-zip', 'application/x-zip-compressed', 'application/zip',
 class	ArchHelperZip(ArchHelper):
 	exts = ('7z',)
 	mimes = ('application/x-7z-compressed',)
-	#                  date              time              attrs                size       csize       name
-	__rx = re.compile("\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [D.][R.][H.][S.][A.] [ 0-9]{12} [ 0-9]{12}  .*\n")
 	#                  size        meth csize     compressn  date              time        crc          name
-	__rx = re.compile("[ 0-9]{8}  .{6} [ 0-9]{8} [ 0-9]{3}% \d{2}-\d{2}-\d{4} \d{2}:\d{2} [0-9a-f]{8}  .*\n")
+	#__rx = re.compile("[ 0-9]{8}  .{6} [ 0-9]{8} [ 0-9]{3}% \d{2}-\d{2}-\d{4} \d{2}:\d{2} [0-9a-f]{8}  .*\n")
+	__rx = QtCore.QRegExp("([ 0-9]{8})  .{6} ([ 0-9]{8}) [ 0-9]{3}% (\\d{2}-\\d{2}-\\d{4} \\d{2}:\\d{2}) [0-9a-f]{8}  ([^\n]*)\n")
  
 	@classmethod
 	def	get_capabilities():
 		return HCAN_LIST
 
-	def	list(self, path, files=list()):
-		'''
-		TODO: common list with re and handler (callback)
-		'''
-		p = subprocess.Popen(["unzip", "-v", path] + files, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-		out, err = p.communicate()
-		if (p.returncode):
-			return (p.returncode, err)
-		retvalue = list()
-		for match in self.__rx.finditer(out):
-			s = match.group().rstrip("\n")
+	@classmethod
+	def	get_list(self, archive):
+		errcode, out, err = self.exec_cmd("unzip", (QtCore.QStringList("-v") << archive))
+		retvalue = []
+		pos = self.__rx.indexIn(out)
+		while (pos != -1):
+			isdir = self.__rx.cap(4).endsWith("/")
 			retvalue.append((
-					s[58:].rstrip('/'),	# name:str
-					(s.endswith('/')),	# isdir:bool
-					datetime.datetime.strptime(s[31:47], "%m-%d-%Y %H:%M"),	# mtime:datetime
-					long(s[:8].lstrip()),		# size:long
-					long(s[17:25].lstrip()),	# csize:long
-				)
-			)
-		return (0, retvalue)
+				self.__rx.cap(4).left(self.__rx.cap(4).length()-1) if isdir else self.__rx.cap(4),		#name
+				isdir,	#isdir
+				QtCore.QDateTime.fromString(self.__rx.cap(3), "MM-dd-yyyy hh:mm"),
+				self.__rx.cap(1).trimmed().toULong()[0],
+				self.__rx.cap(2).trimmed().toULong()[0],
+			))
+			pos += self.__rx.matchedLength()
+			pos = self.__rx.indexIn(out, pos)
+		return (errcode, retvalue)		
 
 mainclass = ArchHelperZip
 
-if __name__ == '__main__':
-	helper = ArchHelperZip()
-	err, values = helper.list(sys.argv[1])
-	for i in values:
-		print i[0], i[1], i[2], i[3], i[4]
+if (__name__ == '__main__'):
+	mainclass.clitest(sys.argv)
